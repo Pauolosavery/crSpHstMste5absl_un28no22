548dcc1479bea8d8822046ce19a5da0779af7991
Pauolo <pauolosavery1@gmail.com>
2022-12-03 22:48:03 +0300

    start

---
+++ src/Components/0-App/App.jsx
@@ -0,0 +1,43 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+// import { useEffect } from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from '../1-NavBar/NavBar.jsx';
+import Home from '../2-Home/Home.jsx';
+import Skill from '../3-Skill/Skill.jsx';
+import Portfolio from '../4-Portfolio/Portfolio.jsx';
+import About from '../5-About/About.jsx';
+import Services from '../6-Services/Services.jsx';
+import Education from '../7-Education/Education.jsx';
+import Achievements from '../8-Achievements/Achievements.jsx';
+import SocialNetwork from '../9-SocialNetwork/SocialNetwork.jsx';
+import Contacts from '../91-Contacts/Contacts.jsx';
+import Footer from '../92-Footer/Footer.jsx';
+// import { fetchGetUser } from "../redux/thunk/thunk";
+
+function App() {
+  // const { user } = useSelector((state) => state.user);
+
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Skill />
+      <Services />
+      <Education />
+      <Achievements />
+      <SocialNetwork />
+      <Contacts />
+      <Footer />
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/1-NavBar/NavBar.jsx
@@ -0,0 +1,4 @@
+import style from './NavBar.module.css';
+export default function NavBar() {
+  return <div className={style.navBar}>NavBar</div>;
+}
