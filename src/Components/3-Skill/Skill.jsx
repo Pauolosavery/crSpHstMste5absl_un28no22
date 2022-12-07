83da4cc662d33e80894ed03f4a1f948ecbf4ac82
Pauolo Savery <pauolosavery1@gmail.com>
2022-12-07 07:25:15 +0000

    Update 3 files

--- src/Components/3-Skill/Skill.jsx
+++ src/Components/3-Skill/Skill.jsx
@@ -8,19 +8,91 @@ export default function Skill() {
           <span className={style.circle} />
           <span className={style.rectengle} />
           <h3>
-            <span className={style.title}>html, css</span>
+            <span className={style.title}>HTML, CSS</span>
             <span className={style.line} />
           </h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p>Tilda, SCSS, Material UI, Fluent UI</p>
         </div>
         <div className={style.single}>
           <span className={style.circle} />
           <span className={style.rectengle} />
           <h3>
-            <span className={style.title}>html, css</span>
+            <span className={style.title}>Figma</span>
             <span className={style.line} />
           </h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p>Photoshop, Illustrator</p>
+        </div>
+        <div className={style.single}>
+          <span className={style.circle} />
+          <span className={style.rectengle} />
+          <h3>
+            <span className={style.title}>JS</span>
+            <span className={style.line} />
+          </h3>
+          <p>TyprScript, ECMAScript 2022, CodeWars - 6 kyu</p>
+        </div>
+        <div className={style.single}>
+          <span className={style.circle} />
+          <span className={style.rectengle} />
+          <h3>
+            <span className={style.title}>Nodejs</span>
+            <span className={style.line} />
+          </h3>
+          <p>Linux, Windows, Express, bcrypt, sessions, webSocket</p>
+        </div>
+        <div className={style.single}>
+          <span className={style.circle} />
+          <span className={style.rectengle} />
+          <h3>
+            <span className={style.title}>React</span>
+            <span className={style.line} />
+          </h3>
+          <p>Class, Hook</p>
+        </div>
+        <div className={style.single}>
+          <span className={style.circle} />
+          <span className={style.rectengle} />
+          <h3>
+            <span className={style.title}>Redux</span>
+            <span className={style.line} />
+          </h3>
+          <p>React-redux, React DevTools, Thunk</p>
+        </div>
+        <div className={style.single}>
+          <span className={style.circle} />
+          <span className={style.rectengle} />
+          <h3>
+            <span className={style.title}>PostgreSQL, mySQL</span>
+            <span className={style.line} />
+          </h3>
+          <p>database, Beekeeper, many-to-many, </p>
+        </div>
+        <div className={style.single}>
+          <span className={style.circle} />
+          <span className={style.rectengle} />
+          <h3>
+            <span className={style.title}>Sequelize</span>
+            <span className={style.line} />
+          </h3>
+          <p>database, Beekeeper, many-to-many</p>
+        </div>
+        <div className={style.single}>
+          <span className={style.circle} />
+          <span className={style.rectengle} />
+          <h3>
+            <span className={style.title}>Docker</span>
+            <span className={style.line} />
+          </h3>
+          <p></p>
+        </div>
+        <div className={style.single}>
+          <span className={style.circle} />
+          <span className={style.rectengle} />
+          <h3>
+            <span className={style.title}>IDE</span>
+            <span className={style.line} />
+          </h3>
+          <p>VScode, npm</p>
         </div>
       </div>
     </div>
