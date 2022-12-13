e43bf3bb3c9a47e5bbd5d6f2a44ef4d83a31bc1a
Pauolo Savery <pauolosavery1@gmail.com>
2022-12-13 11:26:52 +0000

    Update 10 files

--- src/Components/1-NavBar/NavBar.jsx
+++ src/Components/1-NavBar/NavBar.jsx
@@ -1,11 +1,32 @@
+import { useRef } from "react";
 import style from "./NavBar.module.css";
+
 export default function NavBar() {
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle(style.responsive_nav);
+  };
   return (
-    <div className={style.navBar}>
-      <span>NavBar</span>
-      <a href="https://preview.themeforest.net/item/onlyme-one-page-personal-portfolio/full_screen_preview/25624595">
-        DEMO
-      </a>
-    </div>
+    <header>
+      <h3>LOGO</h3>
+      <nav ref={navRef}>
+        <a href="#">Home</a>
+        <a href="#Skill">Мои навыки</a>
+        <a href="#Portfolio">Примеры работ</a>
+        <a href="#About">Обо мне</a>
+        <a href="https://preview.themeforest.net/item/onlyme-one-page-personal-portfolio/full_screen_preview/25624595">
+          дизайн визитки
+        </a>
+        <button
+          className={(style.navBtn, style.navCloseBtn)}
+          onClick={showNavbar}
+        >
+          <div>x</div>
+        </button>
+      </nav>
+      <button className={style.navBtn} onClick={showNavbar}>
+        <div>=</div>
+      </button>
+    </header>
   );
 }
--- src/Components/2-Home/Home.jsx
+++ src/Components/2-Home/Home.jsx
@@ -1,17 +1,13 @@
-import NavBar from '../1-NavBar/NavBar.jsx'
 import style from "./Home.module.css";
 export default function Home() {
   return (
-  <div className={style.home}>
-    <NavBar>
-    <div> LOGO</div>
-    </NavBar>
-    <div> Привет! Я Павел.</div>
-    <div> Fullstack JavaScript developer</div>
-    <div> Мой стек</div>
-    <div> ПОДРОБНЕЕ</div>
-    <div> МОЁ ФОТО</div>
-    <div> СОЦ СЕТИ</div>
-  </div>
-  )
+    <div className={style.home}>
+      <div name="top"> Привет! Я Павел.</div>
+      <div> Fullstack JavaScript developer</div>
+      <div> Мой стек</div>
+      <div> ПОДРОБНЕЕ</div>
+      <div> МОЁ ФОТО</div>
+      <div> СОЦ СЕТИ</div>
+    </div>
+  );
 }
