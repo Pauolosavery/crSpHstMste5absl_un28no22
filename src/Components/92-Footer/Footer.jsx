39d067249d4cc10e3f56e88eed112816042fcc97
Pauolo Savery <pauolosavery1@gmail.com>
2022-12-16 13:47:05 +0000

    Update 9 files

--- src/Components/92-Footer/Footer.jsx
+++ src/Components/92-Footer/Footer.jsx
@@ -1,3 +1,13 @@
+import style from "./Footer.module.css";
 export default function Footer() {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className={style.footer}>
+      <div>Pauolosavery OG </div>
+      <div>2022</div>
+      <div>
+        Вдохновлён{" "}
+        <a href="https://demo.themecon.net/onlyme/html/index-3.html#">Belal</a>
+      </div>
+    </div>
+  );
 }
