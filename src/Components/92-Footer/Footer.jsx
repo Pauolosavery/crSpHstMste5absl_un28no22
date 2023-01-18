c7e290315e15b430669b5e9f702aa2b56e567656
Pauolo Savery <pauolosavery1@gmail.com>
2023-01-18 13:56:27 +0000

    Update 4 files

--- src/Components/92-Footer/Footer.jsx
+++ src/Components/92-Footer/Footer.jsx
@@ -4,48 +4,47 @@ export default function Footer() {
   return (
     <footer className={`${style.footer__section}`}>
       <div className={`${style.container}`}>
-        <div className={`${style.footer__content}`}>
-          <div className={`${style.copyright__text}`}>
-            <p>
-              &#169;{" "}
-              <span className={`${style.update__year}`}>
-                <div>{new Date().getFullYear()}</div>
-              </span>{" "}
-              Сайт создан мной
-              <div>
-                Вдохновлён &#160;
-                <a
-                  href="https://demo.themecon.net/onlyme/html/index-3.html#"
-                  target="_blank"
-                >
-                  Belal
-                </a>
-              </div>
-            </p>
+        <div className={`${style.copyright__text}`}>
+          &#169;{" "}
+          <span className={`${style.update__year}`}>
+            <div>{new Date().getFullYear()}</div>
+          </span>{" "}
+          Сайт создан мной
+          <div>
+            Вдохновлён &#160;
+            <a
+              href="https://demo.themecon.net/onlyme/html/index-3.html#"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Belal
+            </a>
           </div>
-
-          <ul className={`${style.social__icon}`}>
-            <li class="linkedin">
-              <a href="" title="Linkedin" target="_blank">
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="linkedin-in"
-                  class="svg-inline--fa fa-linkedin-in fa-w-14"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
-                  ></path>
-                </svg>
-              </a>
-            </li>
-          </ul>
         </div>
+        <ul className={`${style.social__icon}`}>
+          <li class="linkedin">
+            <a href="" title="Linkedin" target="_blank">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="linkedin-in"
+                class="svg-inline--fa fa-linkedin-in fa-w-14"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                ></path>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a>akuna motata</a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
