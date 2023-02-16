2b9828392dcffee7989afa7f4cc5720a9b33b4e4
Pauolo Savery <pauolosavery1@gmail.com>
2023-02-16 06:53:47 +0000

    Update 12 files

--- src/Components/1-NavBar/NavContacts.jsx
+++ src/Components/1-NavBar/NavContacts.jsx
@@ -1,12 +1,22 @@
 //<a href="https://preview.themeforest.net/item/onlyme-one-page-personal-portfolio/full_screen_preview/25624595">
 import style from "./NavBar.module.css";
 
-export default function NavContacts() {
+export default function NavContacts({ activeMenu }) {
+  // let contcStyle = "";
+  // activeMenu
+  //   ? (contcStyle = `${style.header__contacts} ${style.header__contacts__active}`)
+  //   : (contcStyle = style.header__contacts);
   return (
     <div className={style.header__contacts}>
       <ul>
-        <li>MAIL</li>
-        <li>TELEGRAM</li>
+        <li>
+          <a href="mailto:plekhanov.pavel.job@gmail.com">
+            plekhanov.pavel.job@gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="https://t.me/pavel_trader">Telegram</a>
+        </li>
         <li>LINKEDIN</li>
       </ul>
     </div>
