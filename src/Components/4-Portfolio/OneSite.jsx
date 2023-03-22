006355dd18569bff2e6b1e92d0b6c1993e1e5291
Pauolo Savery <pauolosavery1@gmail.com>
2023-03-22 10:19:43 +0000

    portfolio css done

--- src/Components/4-Portfolio/OneSite.jsx
+++ src/Components/4-Portfolio/OneSite.jsx
@@ -2,19 +2,22 @@ import style from "./OneSite.module.css";
 export default function OneSite({ site, pos }) {
   return (
     <div className={style.single__portfolio}>
-        <a href="#Portfolio">
-      <div className={style.img__area}>
+      <a href="#Portfolio">
+        <div className={style.img__area}>
           <img
             src={require(`../../images/icons/${site.img}`)}
             alt={`${site.title}`}
           />
-      </div>
-        </a>
+        </div>
+      </a>
       <span className={style.number}>{`0${pos}`}</span>
       <div className={style.portfolio__content}>
+        <h2 className={style.portfolio__type}>{site.type}</h2>
         <h2 className={style.portfolio__title}>{site.title}</h2>
-        <h3 className={style.portfolio__myposition}>{site.position}</h3>
-        <p className={style.portfolio__stack}>{site.stack}</p>
+        <h3 className={style.portfolio__myposition}>
+          Позиция: {site.position}
+        </h3>
+        <p className={style.portfolio__stack}>Стек: {site.stack}</p>
         <p className={style.portfolio__discription}>{site.discription}</p>
         <a className={style.portfolio__single__link} href="#Home">
           Посетить
