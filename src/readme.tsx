9084dbd907866b746f4ddd1cff0d09f2674d9822
Pauolo <pauolosavery1@gmail.com>
2025-01-01 14:00:00 +0300

    1st commit

---
+++ src/readme.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ReadMe = () => {
+
+    return (
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum veritatis cupiditate voluptatum, sunt rerum sint eaque debitis tempore iste blanditiis, id ipsum illo perspiciatis cumque aliquam expedita nobis! Nihil, mollitia.
+            Ratione harum repudiandae voluptas consequatur explicabo eum est labore optio sint illum, molestias corrupti alias. Odit dolores, natus molestiae exercitationem nulla reiciendis eius, assumenda aspernatur nobis laudantium hic vel pariatur.
+            Quisquam ducimus porro nostrum? Harum accusantium, explicabo doloremque fugiat facere quae neque quas repellendus accusamus voluptates eum in deserunt animi blanditiis illo! Ut reiciendis in similique corrupti impedit voluptatem eveniet.
+            Libero magnam facere expedita delectus ipsam vero voluptatibus optio error eaque iste ut quos itaque inventore laborum quam veniam recusandae eveniet, placeat aliquid dignissimos quaerat ex sed autem! Reprehenderit, ea?
+            Eum dolorem deleniti necessitatibus, aut magni illum libero iure provident explicabo sunt impedit. Commodi, placeat quia! Perspiciatis unde recusandae praesentium ab impedit. Facere reiciendis ullam numquam perferendis et inventore ipsam.
+            Repellat excepturi tempora dolores obcaecati quas sit quibusdam necessitatibus dolorum iste sequi nobis temporibus, vero ea nisi esse deleniti inventore odio rem cum neque voluptatibus sapiente dignissimos! Iusto, impedit animi!
+            A ducimus unde provident illum. Tempora assumenda dolor hic blanditiis nobis ratione, expedita eum. Optio porro numquam, nemo ullam accusamus aut. Porro soluta exercitationem consectetur aliquam, odit tempora obcaecati commodi.
+            Perspiciatis sunt neque fugit, numquam placeat rem. Commodi provident neque consequatur esse, quod natus repudiandae eaque dicta accusamus distinctio et nobis consectetur! Ad deserunt nam, animi non ipsum quam praesentium.
+            Laboriosam iste officiis sequi voluptatum magnam numquam expedita quis quasi eos quisquam maiores officia earum aliquam, sapiente hic eum fugit quam! Excepturi perferendis sed iusto iure vero culpa consequuntur facilis!
+            Porro quisquam, officiis nam maiores, provident vero eligendi magnam natus iste recusandae eveniet repellat, inventore quam sapiente! Rerum, blanditiis? Itaque nulla voluptatum odio? Error, dolor qui cumque inventore excepturi omnis.
+            Corporis, atque amet! Perspiciatis numquam repellendus quidem id hic rem provident molestias atque? Impedit neque beatae harum reiciendis, perspiciatis optio quis, quasi magnam mollitia dicta id sapiente itaque blanditiis. Tenetur.
+            Minus nesciunt quis molestiae ad id soluta deleniti, fugiat labore dignissimos non perspiciatis obcaecati amet eligendi quam consequatur neque aut qui placeat voluptates unde beatae debitis? Doloribus maxime magnam molestias.
+            Accusantium perferendis nam soluta, minus ut incidunt quidem minima perspiciatis harum expedita inventore fuga est! Necessitatibus omnis magnam sit aliquid quas porro autem et, voluptatem, temporibus, veritatis rerum eaque repellendus!
+            Accusantium aspernatur obcaecati ut culpa magnam veniam quo amet debitis nulla! Consectetur nobis velit, sit dolorum consequuntur aut laborum asperiores, architecto molestiae laboriosam modi inventore fugit corrupti esse assumenda ipsum.
+            Odio officia praesentium voluptatum nobis dolorum reprehenderit est iusto, delectus velit maxime! Rerum eveniet adipisci repudiandae quam aperiam animi nihil at! Consectetur vero error cum necessitatibus totam placeat deleniti fuga.
+            A explicabo suscipit quod omnis quidem laboriosam saepe, autem accusantium, aut cum officiis! Consectetur tempora amet porro quod delectus, deleniti necessitatibus. Doloremque facere suscipit ab nam accusamus perferendis facilis minus.
+            Eum, eos veniam. Sit dicta laboriosam ea quae dolore doloremque, quasi alias veniam a hic molestias? Non minima laborum rerum sunt nobis eius unde facere ipsum, inventore reiciendis sed modi?
+            Voluptatem illo praesentium saepe eaque animi. Perferendis officiis debitis soluta provident, eos facilis rerum ea repellendus quo. Neque, voluptatum placeat doloremque vero iusto modi. Cum distinctio dignissimos quibusdam nam quam?
+            Ducimus temporibus sequi ipsam? Minus ipsa blanditiis sunt corrupti delectus rerum, reprehenderit beatae sapiente possimus sint odit tenetur dolor, temporibus ex, neque molestias veniam hic excepturi eum autem debitis. Distinctio.
+            Alias voluptas, ipsam ex eligendi quae soluta molestias obcaecati magnam sapiente aperiam. Reiciendis voluptas non autem culpa omnis nobis sunt repudiandae explicabo aperiam cum, deleniti, recusandae blanditiis voluptate consequuntur vel.
+        </p>
+    )
+}
+
+export default ReadMe
